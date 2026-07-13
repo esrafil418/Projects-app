e5434a930d9a5490a3ab5f53c7b4fefdfda6a8c3
Esrafil <esrafil.s.r.man418@gmail.com>
2026-07-13 23:26:25 +0330

    feat: Create Navbar component and Nav links in constans

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load more</h1>
+    </section>
+  );
 }
